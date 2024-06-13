1695906b3ad5a24727f0a39ce936a2b419acae47
nuytsm <marknuyts@gmail.com>
2024-06-13 11:05:15 +0200

    index pagina aangepast

--- python/GoogleAnalyticsDocinfoProcessor.rb
+++ python/GoogleAnalyticsDocinfoProcessor.rb
@@ -1,17 +1,24 @@
+require 'asciidoctor/extensions'
+
 class GoogleAnalyticsDocinfoProcessor < Asciidoctor::Extensions::DocinfoProcessor
-    use_dsl
-    at_location :footer
-    def process document
-      #return unless (ga_account_id = document.attr 'google-analytics-account')
-      %(<!-- Google tag (gtag.js) -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-3K061L13YN"></script>
+  use_dsl
+  at_location :footer
+
+  def process(document)
+    google_analytics_account_id = document.attr('google_analytics_account_id')
+    puts "Google Analytics Account ID: #{google_analytics_account_id}"
+    return unless google_analytics_account_id
+
+    %(<script async src="https://www.googletagmanager.com/gtag/js?id=#{google_analytics_account_id}"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-      
-        gtag('config', 'G-3K061L13YN');
-      </script>
-      )
-    end
+        gtag('config', '#{google_analytics_account_id}');
+      </script>)
   end
+end
+
+Asciidoctor::Extensions.register do
+  docinfo_processor GoogleAnalyticsDocinfoProcessor
+end
