d602a6bf7d9e0dce352a3bdee8d3bafe27258a2f
nuytsm <nuytsm@users.noreply.github.com>
2025-05-26 09:32:26 +0000

    deploy: d58d1977e945c9bb92652b659be696c22271af6a

--- python/dropdown.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.collapsible').forEach(item => {
-      item.addEventListener('click', function() {
-        const content = this.nextElementSibling;
-        content.style.display = (content.style.display === 'block') ? 'none' : 'block';
-      });
-    });
-  });
--- python/extensions.rb
+++
@@ -1,5 +0,0 @@
-Asciidoctor::Extensions.register do
-    docinfo_processor GoogleAnalyticsDocinfoProcessor
-  end
-  
-  Asciidoctor.convert_file 'index.adoc', safe: :safe
